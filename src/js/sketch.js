eab07760e708802ec20fe33a19f7c579a36e6558
JadenLeung <jaden.leung@gmail.com>
2023-09-04 17:17:41 -0400

    test

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -535,7 +535,7 @@ p.setup = () => {
 	INPUT.option("Key-3x3x2");
 	INPUT.option("Key-Gearcube");
 	INPUT.option("Button");
-	if(('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) && !matchMedia('(pointer:fine)').matches) //phone computer
+	if(window.matchMedia("(max-width: 767px)").matches || isIpad()) 
 		INPUT.selected('Button');
 	else
 		INPUT.selected('Keyboard');
@@ -4581,7 +4581,7 @@ p.keyPressed = (event) => {
 	}
 	if(p.keyCode == 16){ //shift
 		setLayout();
-		console.log(isIpad());
+		console.log("hello");
 	}
 	if(customb > 0 && (p.keyCode <37 || p.keyCode > 40)) return;
 
@@ -8241,8 +8241,7 @@ $(document).on("keypress", "input", function(e){
 	}
 });
 function isIpad(){
-	return ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) && !(window.matchMedia("(max-width: 767px)").matches) 
-	&& !matchMedia('(pointer:fine)').matches;
+	return ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) && !(window.matchMedia("(max-width: 767px)").matches);
 }
 
 function sideSolved(color)
