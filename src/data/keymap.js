0464d1ad5d24f6a64bf3abeab45ecb577aaa6169
JadenLeung <jaden.leung@gmail.com>
2025-08-23 22:33:19 -0700

    fixed bug

--- src/data/keymap.js
+++ src/data/keymap.js
@@ -9,12 +9,14 @@ export let keymappings = {
         5: "M", 6: "M",
         q: "z'", w: "B", e: "L'", r: "Lw'", t: "x", y: "x", u: "Rw", i: "R", o: "B'", p: "z",
         a: "y'", s: "D", d: "L", f: "U'", g: "F'", h: "F", j: "U", k: "R'", l: "D'", ";": "y",
-        z: "Dw", x: "M'", c: "Uw'", v: "Lw", b: "x'", n: "x'", m: "Rw'", ",": "Uw", ".": "M'", "/": "Dw'"
+        z: "Dw", x: "M'", c: "Uw'", v: "Lw", b: "x'", n: "x'", m: "Rw'", ",": "Uw", ".": "M'", "/": "Dw'",
+        arrowleft: "y", arrowright: "y'", arrowdown: "x'", arrowup: "x"
     },
     Custom: {
         5: "M", 6: "M",
         q: "z'", w: "B", e: "L'", r: "Lw'", t: "x", y: "x", u: "Rw", i: "R", o: "B'", p: "z",
         a: "y'", s: "D", d: "L", f: "U'", g: "F'", h: "F", j: "U", k: "R'", l: "D'", ";": "y",
-        z: "Dw", x: "M'", c: "Uw'", v: "Lw", b: "x'", n: "x'", m: "Rw'", ",": "Uw", ".": "M'", "/": "Dw'"
+        z: "Dw", x: "M'", c: "Uw'", v: "Lw", b: "x'", n: "x'", m: "Rw'", ",": "Uw", ".": "M'", "/": "Dw'",
+        arrowleft: "y", arrowright: "y'", arrowdown: "x'", arrowup: "x"
     },
 };
