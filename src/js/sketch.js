ef367115f9f93d87ec76c30209d69a3eb93d0822
JadenLeung <jaden.leung@gmail.com>
2025-08-25 15:38:07 -0400

    fixed saving

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -5398,7 +5398,7 @@ function setSettings(obj) {
 	HOLLOW.checked(obj.hollow == 1);
 	let d = obj.background;
 	localStorage.background = d;
-	localStorage.keymappings = obj.keymappings;
+	keymappings = JSON.parse(obj.keymappings);
 	if (obj.bandaged3 && obj.bandaged3 != "null") {
 		localStorage.bandaged3 = obj.bandaged3;
 		bandaged3 = JSON.parse(localStorage.bandaged3);
